60341219d00d9b732f1682f83c4ba27dee8e6aa8
Doug Black <dblack@americanbible.org>
2018-01-16 17:07:14 -0500

    Adding functions

--- requires/GrabController.php
+++ requires/GrabController.php
@@ -3,11 +3,18 @@
 class GrabController()
 {
 	
-	private $awsMessages;
+	public $awsMessages;
+
+	public $databaseInfo;
+
+	public $dbTable;
+
+	public $db;
 
 	function __construct()
 	{
 
+		// AWS Messages
 		$this->awsMessages = array(
 			"Service is operating normally",
 			"Informational message",
@@ -15,8 +22,24 @@ class GrabController()
 			"Service disruption"
 		);
 
+		// Database info
+		require_once('../config/database.php');
+		$this->databaseInfo = $dataconnection;
+
+		// Set DB table
+		$this->dbTable = 'aws_data';
+
 	}
 
-	public function 
+	public function establishDBConnection()
+	{
+
+
+	}
+
+	private function createTable()
+	{
+
+	}
 
 }
