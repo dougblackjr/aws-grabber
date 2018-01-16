75d6369c09c29aba0041e3cb90a8143db1904cb8
Doug Black <dblack@americanbible.org>
2018-01-16 16:59:42 -0500

    Initial controller

--- requires/GrabController.php
+++ requires/GrabController.php
@@ -3,4 +3,20 @@
 class GrabController()
 {
 	
+	private $awsMessages;
+
+	function __construct()
+	{
+
+		$this->awsMessages = array(
+			"Service is operating normally",
+			"Informational message",
+			"Service degradation",
+			"Service disruption"
+		);
+
+	}
+
+	public function 
+
 }
