b2d2a29e4289b64dcb22405a70c8acfa1fcb6e33
Doug Black <dblack@americanbible.org>
2018-01-17 18:23:10 -0500

    Error checking

--- config/feeds.md
+++ config/feeds.md
@@ -1,2 +1,2 @@
-dddhttps://status.aws.amazon.com/rss/ec2systemsmanager-us-east-1.rss
+https://status.aws.amazon.com/rss/ec2systemsmanager-us-east-1.rss
 https://status.aws.amazon.com/rss/appstream2-us-east-1.rss
--- script/getdata.php
+++ script/getdata.php
@@ -1,22 +1,52 @@
 <?php
 
 // Initialize variables
-define("DOCROOT", getcwd());
+$rootDir = realpath(dirname(__FILE__) . '/..');
+define("DOCROOT", $rootDir);
 require_once(DOCROOT . '/requires/GrabController.php');
 
 $grabber = new GrabController();
 
-// Get name of script via $argv
-foreach ($argv as $key => $value) {
-	
-	// Skip the first one
-	if ($key == 0) continue;
-	
-	// Parse
-	$response = $grabber->parse($value);
 
-	echo $response . PHP_EOL;
+// If there are no arguments, we'll read from file
+if(!isset($argv[1]) && file_exists(DOCROOT . '/config/feeds.md')) {
+	// Read from file, throw it in an array
+	$file = fopen(DOCROOT . '/config/feeds.md', 'r');
+	if(filesize(DOCROOT . '/config/feeds.md') > 0) {
+		$filecontents = fread($file, filesize(DOCROOT . '/config/feeds.md'));
+	} else {
+		echo "No feeds in file. Either add feeds to feeds.md, or use the CLI!" . PHP_EOL;
+		die();
+	}
+	fclose($file);
 
+	$feeds = explode("\n", $filecontents);
+
+	echo "I see " . count($feeds) . " feeds to grab." . PHP_EOL;
+
+	foreach ($feeds as $feed) {
+		// Parse feed
+
+		$response = $grabber->parse($feed);
+
+		echo $response . PHP_EOL;
+	}
+	
+} else {
+	// Get names of script via $argv
+	foreach ($argv as $key => $value) {
+		
+		// Skip the first one
+		if ($key == 0) continue;
+		
+		// Parse
+		$response = $grabber->parse($value);
+
+		echo $response . PHP_EOL;
+
+	}
 }
-// Curl it and put it in and parse xml
-// Throw it in the DB (Date, Status, Message)
+
+echo "Grabber has completed! Now, hire Doug Black!" . PHP_EOL;
+
+// END OF FILE
