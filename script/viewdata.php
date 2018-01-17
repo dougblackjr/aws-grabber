405f1fa65f56e889a0e8e84bd5d187217a621e55
Doug Black <dblack@americanbible.org>
2018-01-17 15:03:43 -0500

    Added sanitization

--- script/viewdata.php
+++ script/viewdata.php
@@ -8,7 +8,7 @@ require_once(DOCROOT . '/requires/ViewController.php');
 $viewer = new ViewController();
 
 // Get data
-$channel = isset($_GET['channel']) ? $_GET['channel'] : null;
-$offset = isset($_GET['offset']) ? $_GET['offset'] : 0;
+$channel = isset($_GET['channel']) ? filter_var($_GET['channel'], FILTER_SANITIZE_STRING) : null;
+$offset = isset($_GET['offset']) ? (!filter_var($_GET['offset'], FILTER_SANITIZE_NUMBER_INT) === FALSE ? filter_var($_GET['offset'], FILTER_SANITIZE_NUMBER_INT) : 0) : 0;
 
 echo $viewer->view($channel, $offset);
